273b11dd2763e8d451040df0f4ada4ae5d8c7c29
Garrett Rowell <garrett@puppet.com>
2024-09-25 16:25:31 -0600

    this?

--- lib/puppet_x/garrettrowell/awss3/s3object.rb
+++ lib/puppet_x/garrettrowell/awss3/s3object.rb
@@ -1,22 +1,25 @@
 # Based off of example at: https://github.com/awsdocs/aws-doc-sdk-examples/blob/main/ruby/example_code/s3/object_get.rb
-module PuppetX::Garrettrowell::Awss3
-  class S3ObjectGetWrapper
-    attr_reader :object
+module PuppetX
+  module Garrettrowell
+    module Awss3
+      class S3ObjectGetWrapper
+        attr_reader :object
 
-    # @param object [Aws::S3::Object] An existing Amazon S3 object.
-    def initialize(object)
-      @object = object
+        # @param object [Aws::S3::Object] An existing Amazon S3 object.
+        def initialize(object)
+          @object = object
+        end
+
+        # Gets the object directly to a file.
+        #
+        # @param target_path [String] The path to the file where the object is downloaded.
+        # @return [Aws::S3::Types::GetObjectOutput, nil] The retrieved object data if successful; otherwise nil.
+        def get_object(target_path)
+          @object.get(response_target: target_path)
+        rescue Aws::Errors::ServiceError => e
+          raise Puppet::Error, _("Couldn't get object %{object}. Here's why: %{detail}") % { object: @object.key, detail: e.message }, e.context
+        end
+      end
     end
-
-    # Gets the object directly to a file.
-    #
-    # @param target_path [String] The path to the file where the object is downloaded.
-    # @return [Aws::S3::Types::GetObjectOutput, nil] The retrieved object data if successful; otherwise nil.
-    def get_object(target_path)
-      @object.get(response_target: target_path)
-    rescue Aws::Errors::ServiceError => e
-      raise Puppet::Error, _("Couldn't get object %{object}. Here's why: %{detail}") % { object: @object.key, detail: e.message }, e.context
-    end
-
   end
 end
